585da5ee09b16ca0b7e5369522a5b157ae15ff19
torokokoo <torokokov7@gmail.com>
2022-09-19 01:18:18 -0300

    fix error de codigo hexadecimal

--- pruebas_20807369_ToroBarrenechea.rkt
+++ pruebas_20807369_ToroBarrenechea.rkt
@@ -4,11 +4,12 @@
 (require "TDApixrgb_20807369_ToroBarrenechea.rkt")
 (require "TDApixhex_20807369_ToroBarrenechea.rkt")
 (require "TDAimage_20807369_ToroBarrenechea.rkt")
+(require "TDAhistogram_20807369_ToroBarrenechea.rkt")
 
 ; Datos para probar
 (define bit-image (image 2 2 (pixbit-d 1 1 0 0) (pixbit-d 1 2 1 0) (pixbit-d 2 1 0 1) (pixbit-d 2 2 1 1)))
 (define rgb-image (image 2 2 (pixrgb-d 1 1 0 0 0 0) (pixrgb-d 1 2 50 50 50 0) (pixrgb-d 2 1 100 100 100 1) (pixrgb-d 2 2 200 200 200 1)))
-(define hex-image (image 2 2 (pixhex-d 1 1 "FFFFFF" 0) (pixhex-d 1 2 "000000" 0) (pixhex-d 2 1 "FCBA03" 1) (pixhex-d 2 2 "#FC03A5" 1)))
+(define hex-image (image 2 2 (pixhex-d 1 1 "FFFFFF" 0) (pixhex-d 1 2 "000000" 0) (pixhex-d 2 1 "FCBA03" 1) (pixhex-d 2 2 "FC03A5" 1)))
 
 (define bit-image-xl
   (image 4 4
